ea5c2895cb1a7d7b6d4ee09c0f3af8e29151214f
Afiq Hatta <ahmadafiqhatta@ahmadafiqhattas-MacBook-Pro.local>
2022-01-01 05:48:49 +0000

    port change

--- main.py
+++ main.py
@@ -2,15 +2,28 @@ import os
 from flask import Flask, render_template
 from apscheduler.schedulers.background import BackgroundScheduler
 import numpy as np
+import pandas as pd
+import data_process
 
 
 app = Flask(__name__)
+
 
 def sensor():
     """ Function for test purposes. """
     with open("static/time-log.txt", "w") as myfile:
         myfile.write("TIME: {}".format(np.random.randint(10)))
     print("Scheduler is alive!")
+
+
+def save_latest_frequencies():
+    df = data_process.count_article_daily_freq(pd.read_pickle('flood_dat.pickle'))
+    df.to_csv('static/news_frequencies.csv')
+    print('Saving news frequencies')
+
+
+def pull_latest():
+    pass
 
 
 @app.route("/")
@@ -24,6 +37,7 @@ if __name__ == "__main__":
     sensor()
     sched = BackgroundScheduler(daemon=True)
     sched.add_job(sensor, 'interval', seconds=2)
+    sched.add_job(save_latest_frequencies, 'interval', seconds=2)
     sched.start()
 
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 9090)))
