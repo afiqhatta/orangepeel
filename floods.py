658c91ed4c3ec779cf8892620624e0f29771f1ee
Afiq Hatta <ahmadafiqhatta@ahmadafiqhattas-MacBook-Pro.local>
2022-01-04 22:00:15 +0000

    Scheduling and logging

--- floods.py
+++ floods.py
@@ -11,6 +11,9 @@ def get_links(url):
 
     link_list = []
     data = soup.findAll('div',attrs={'class':'articles'})
+    # data = soup.findAll('main',attrs={'class':'site-main'}) # floodlist
+    print(data)
+
     for div in data:
         links = div.findAll('a')
         for a in links:
@@ -23,23 +26,25 @@ def get_links(url):
 def unwrap_data(urls):
     dat = NewsPlease.from_urls(urls, timeout=6)
     for key, value in dat.items():
-        dat[key] = [value.title, value.date_publish, value.description, value.maintext, value.authors]
+        dat[key] = [value.title, value.date_publish, value.description, value.authors]
     return dat
 
 
 def parse_into_frame(url):
     urls = get_links(url)
+    print(urls)
     df = pd.DataFrame(unwrap_data(urls)).T
-    df.columns = ['Title', 'Date', 'Description', 'Text', 'Authors']
+    df.columns = ['Title', 'Date', 'Description', 'Authors']
     return df
 
 
-def parse_multiple_pages(base_url, pages=15):
+def parse_multiple_pages(base_url, pages=15, init_page=1, extension=None):
 
+    # take a set of links in a page, and then scrape them
     full_dat = []
     page_urls = ([base_url]
                  + ['{base_url}&page={i}'.format(base_url=base_url, i=i) for i in
-                    range(1, pages)])
+                    range(init_page, pages)])
 
     for url in page_urls:
         print(url)
@@ -48,10 +53,51 @@ def parse_multiple_pages(base_url, pages=15):
     return pd.concat(full_dat)
 
 
+def parse_multiple_pages_from_urls(page_urls, pages=15, init_page=1, extension=None):
+    full_dat = []
+
+    for url in page_urls:
+        print(url)
+        full_dat.append(parse_into_frame(url))
+
+    return pd.concat(full_dat)
+
+
+def update_reliefweb(urls, prefix='flood'):
+
+    for country, url in urls.items():
+        print('updating ')
+        save_string = 'metadata/flood_{}.pickle'.format(country)
+        df = pd.read_pickle(save_string)
+        updated_df = pd.concat([df, parse_multiple_pages(base_url=url, pages=2)])
+        updated_df.to_pickle(save_string)
+
+    return 0
+
+
 
 if __name__=='__main__':
 
-    urls = {'Malaysia': 'https://reliefweb.int/updates?advanced-search=%28PC147%29_%28DT4611%29',
-            'Philippines': 'https://reliefweb.int/updates?advanced-search=%28C188%29_%28DT4611%29'}
+    floodlist_url = {'Malaysia': 'https://floodlist.com/tag/Malaysia',
+                      'Thailand': 'https://floodlist.com/tag/Thailand',
+                      'Vietnam': 'https://floodlist.com/tag/Vietnam',
+                      'Indonesia': 'https://floodlist.com/tag/Indonesia',
+                      'Philippines': 'https://floodlist.com/tag/Philippines'}
 
-    print(parse_multiple_pages(base_url=urls['Philippines'], pages=2))
+    floodlist_pattern = 'https://floodlist.com/tag/'
+
+    # do this to load url from relief webdriver
+    # for country, url in urls.items():
+    #     parse_multiple_pages(base_url=url, pages=2).to_pickle('metadata/flood_{}.pickle'.format(country))
+
+
+    # # do this for floodlist
+    # for country, url in floodlist_url.items():
+    #     url_list = [url] + ['{base_url}/page/{i}'.format(base_url=url, i=i) for i in range(2, 3)]
+    #     print(url_list)
+    #     parse_multiple_pages_from_urls(url_list).to_pickle('metadata/floodlist_{}.pickle'.format(country))
+    #
+    # print(pd.read_pickle('metadata/floodlist_Malaysia.pickle'))
+
+    # update
+    update_reliefweb(urls)
--- main.py
+++ main.py
@@ -4,9 +4,29 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import numpy as np
 import pandas as pd
 import data_process
+from datetime import datetime
+import floods
 
 
 app = Flask(__name__)
+
+
+def update_metadata():
+
+    urls = {'Malaysia': 'https://reliefweb.int/updates?advanced-search=%28PC147%29_%28DT4611%29',
+            'Philippines': 'https://reliefweb.int/updates?advanced-search=%28C188%29_%28DT4611%29',
+            'Indonesia': 'https://reliefweb.int/updates?advanced-search=%28PC120%29_%28DT4611%29'}
+
+    floods.update_reliefweb(urls)
+    logdownload()
+    return 0
+
+
+def logdownload():
+    # log everytime a job is done
+    with open("metadata/logtime.txt", "a") as myfile:
+        myfile.write("TIME: {} \n".format(datetime.now()))
+    print("Logged!")
 
 
 def sensor():
@@ -22,10 +42,6 @@ def save_latest_frequencies():
     print('Saving news frequencies')
 
 
-def pull_latest():
-    pass
-
-
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
@@ -34,10 +50,13 @@ def hello_world():
 
 
 if __name__ == "__main__":
+
     sensor()
     sched = BackgroundScheduler(daemon=True)
     sched.add_job(sensor, 'interval', seconds=2)
-    sched.add_job(save_latest_frequencies, 'interval', seconds=2)
+    sched.add_job(logdownload, 'interval', seconds=10)
+    sched.add_job(update_metadata, 'interval', seconds=10*60)
+
     sched.start()
 
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 9090)))
